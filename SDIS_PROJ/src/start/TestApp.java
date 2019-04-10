da8d2b6ba650fcf4ba72e4cb04987bb7761fe360
JoaoAlvaroFerreira <JoaoAlvaroFerreira@FIREBOY.localdomain>
2019-04-10 09:42:44 +0100

    mid-aula

--- SDIS_PROJ/src/start/TestApp.java
+++ SDIS_PROJ/src/start/TestApp.java
@@ -19,35 +19,24 @@ public class TestApp {
 	private static String file_path;
 	private static double disk_space;
 	private static int replication_degree;
-	
     public static void main(String[] args) throws UnknownHostException { //<peer_ap> <sub_protocol> <opnd_1> <opnd_2> 
     	
     	peer_ap = args[0];
         sub_protocol = args[1].toUpperCase();
-
+        System.out.println(args[0]);
         
-        // Create a buffer of bytes, which will be used to store
-        // the incoming bytes containing the information from the server.
-        // Since the message is small here, 256 bytes should be enough.
-        byte[] buf = new byte[256];
-
-    	String host = (args.length < 1) ? null : args[0];
-         
-        // Create a new Multicast socket (that will allow other sockets/programs
-        // to join it as well.
         try {
-            //Joint the Multicast group.
+         
         	
-        	  Registry registry = LocateRegistry.getRegistry("localhost");
+        	Registry registry = LocateRegistry.getRegistry("localhost");
       	    RMI stub = (RMI) registry.lookup(peer_ap);
-      	    //String response = stub.sayHello();
-      	    //System.out.println("response: " + response);
-      	    
+              System.out.println(args[0]);
         } catch (Exception e) {
-        	System.err.println("Client exception: " + e.toString());
+        	System.err.println("TestApp exception: " + e.toString());
        	    e.printStackTrace();
         }
         
+        System.out.println(args[0]);
        if(sub_protocol == "BACKUP")
        {
     	   file_path = args[2];
@@ -65,6 +54,7 @@ public class TestApp {
     	   disk_space = Double.parseDouble(args[2]);
     	   replication_degree = Integer.parseInt(args[3]);
        }
+       System.out.println(args[0]);
         
     }
 }
